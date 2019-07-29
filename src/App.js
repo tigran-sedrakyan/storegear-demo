23abe02eb735586d15a2c04abdb5b00750f6d436
Tigran Sedrakyan <tikona.sedrakyan@gmail.com>
2019-07-29 17:00:59 +0400

    removed useless files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider} from '@material-ui/styles';
+import theme from './theme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Tabs from './components/atoms/tabs';
+import List from './components/organisms/list';
+import Map from './components/molecules/map';
+
+import stops from './stops';
+
+export default function App() {
+    const [data, setData] = React.useState(stops.entries);
+
+    // handles finish of a stop
+    function handleFinish(id) {
+        const newData = [...data];
+        const index = newData.findIndex((el) => el.id === id);
+        newData[index] = {...newData[index], status: 'finished'};
+        if (index < newData.length - 1) {
+            newData[index + 1] = {...newData[index + 1], status: 'inprogress'};
+        }
+        setData(newData);
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Tabs tabs={[`Stops (${data.length})`, 'Map']}>
+                {(activeTab) =>
+                    !activeTab ? (
+                        <List stops={data} handleFinish={handleFinish} />
+                    ) : (
+                        <Map stops={data} handleFinish={handleFinish} />
+                    )
+                }
+            </Tabs>
+        </ThemeProvider>
+    );
 }
-
-export default App;
